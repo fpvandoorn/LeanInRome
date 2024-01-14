064474a5cae5deccf7356aea1e4aa08d4fd3635e
Floris van Doorn <fpvdoorn@gmail.com>
2024-01-14 23:48:55 +0100

    fix some files, rename folders

--- lakefile.lean
+++ lakefile.lean
@@ -1,17 +1,14 @@
 import Lake
 open Lake DSL
 
-def serverOptions : Array LeanOption := #[
+package «leanInRome» where
+  leanOptions := #[
     ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
     ⟨`autoImplicit, false⟩,
-    ⟨`relaxedAutoImplicit, false⟩
-  ]
-
-package «leanInRome» where
-  moreServerOptions := serverOptions
-
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"@"e659b1b3c9ad1535ef5f4ec13280a382fe457aee"
+    ⟨`relaxedAutoImplicit, false⟩]
 
 @[default_target]
 lean_lib «LeanInRome» where
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"@"e659b1b3c9ad1535ef5f4ec13280a382fe457aee"
